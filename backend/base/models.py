ea46b7170df9a8a83e6a744c9c08f992462c4efe
Mariusz <mk.11.03@wp.pl>
2022-03-10 04:40:56 +0100

    Refactor

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,9 +1,5 @@
-from operator import mod
-from pydoc import describe
 from django.db import models
 from django.contrib.auth.models import User
-
-# Create your models here.
 
 
 class Product(models.Model):
